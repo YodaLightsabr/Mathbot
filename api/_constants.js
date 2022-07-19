d93249d96e6402ad5a69f4f1c25f23b81af85f20
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-19 02:18:50 -0700

    fix

--- api/_constants.js
+++
@@ -1,3 +0,0 @@
-// These can be filled after Slack configuration
-export const token = process.env.SLACK_BOT_TOKEN
-export const signingSecret = process.env.SLACK_SIGNING_SECRET
--- api/event_handlers/_challenge.js
+++
@@ -1,6 +0,0 @@
-export function challenge(req, res) {
-
-    res.status(200).send({
-        "challenge": req.body.challenge
-    })
-}
